568de8ec38aed10e06fd4f88199677ed360041ae
JamyDon <83012616+JamyDon@users.noreply.github.com>
2023-10-07 22:24:41 +0800

    Update edit_combination.py

--- ensemble/edit_combination.py
+++ ensemble/edit_combination.py
@@ -12,7 +12,8 @@ from pytorch_pretrained_bert import BertTokenizer
 from transformers.models.gpt2 import GPT2Config, GPT2LMHeadModel
 from PLM.gpt2_scorer import sentence_probability, padding
 from m2convertor import generate_tgt
-from traditional import get_models, parse_m2, validate
+from traditional import get_models
+from rule_ensemble import parse_m2, validate
 import torch
 
 
